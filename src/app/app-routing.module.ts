a5af4e46bf01efacf85f4fcc2203ddfe9e22e4fc
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2022-11-12 00:29:47 -0400

    mudanças

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { AuthenticationGuard } from './guards/authentication.guard';
-import { PageComponent } from './layout/page/page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { PageComponent } from './layout/page/page.component';
 import { CursoCreateComponent } from './pages/curso/curso-create/curso-create.component';
-import { ProfessorCursoComponent } from './pages/curso/curso-list/professor-curso.component';
+import { CursoComponent } from './pages/curso/curso-list/curso.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ProfessorHomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -16,11 +16,11 @@ const routes: Routes = [
     canActivate: [AuthenticationGuard],
     canActivateChild: [AuthenticationGuard],
     children: [
-      { path: '', component: ProfessorHomeComponent },
+      { path: '', component: HomeComponent },
       {
         path: 'cursoProfessor',
         children: [
-          { path: '', component: ProfessorCursoComponent },
+          { path: '', component: CursoComponent },
           { path: 'create', component: CursoCreateComponent },
         ],
       },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,36 @@
-import { JsonDateInterceptor } from './interceptors/json-date.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppRedDirective } from './directives/appRed.directive';
-import { CursoComponent } from './pages/curso/curso-list/curso.component';
-import { HomeComponent } from './pages/home/home.component';
+import { JsonDateInterceptor } from './interceptors/json-date.interceptor';
+import { JwtAuthInterceptor } from './interceptors/jwt-auth.interceptor';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { PageComponent } from './layout/page/page.component';
 import { CursoCreateComponent } from './pages/curso/curso-create/curso-create.component';
 import { CursoDeleteComponent } from './pages/curso/curso-delete/curso-delete.component';
-import { PageComponent } from './layout/page/page.component';
+import { CursoComponent } from './pages/curso/curso-list/curso.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { JwtAuthInterceptor } from './interceptors/jwt-auth.interceptor';
 
 @NgModule({
   declarations: [
